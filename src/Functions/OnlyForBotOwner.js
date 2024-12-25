2063bf463ece3ee946b9cbd86ff11b11d5c556d7
muhammadbinasim471 <muhammadbinasim471@gmail.com>
2024-12-25 11:21:21 +0000

    Re-Added eslint cuz i am indecisive

--- src/Functions/OnlyForBotOwner.js
+++ src/Functions/OnlyForBotOwner.js
@@ -3,30 +3,30 @@ module.exports = [
     name: "onlyForBotOwner",
     params: ["slash"],
     code: `
-        $return[
-            $onlyIf[$or[$env[slash]==true;$env[slash]==false];Custom Function onlyForBotOwner isn't being used properly! Use true or false instead of $env[slash] in parameters.
-                $deleteIn[5s]]
-            $onlyIf[$authorID==$botOwnerID;
-                $ifx[
-                    $if[$env[slash==true;$ephemeral
-                        $color[#ff694b]
-                        $title[$toTitleCase[not authorized to use this command]]
-                        $description[Only $username[$botOwnerID] is authorized to use this command.
-                            But hey... why don't you come over tonight and i will authorize you something *else* 💋💋
-                        ]
-                    ]
-                    $else[
-                        $color[#ff694b]
-                        $title[$toTitleCase[not authorized to use this command]]
-                        $description[Only $username[$botOwnerID] is authorized to use this command.
-                            But hey... why don't you come over tonight and i will authorize you something *else* 💋💋
-                        ]$deleteIn[15s]
-                        $wait[15s]
-                        $deleteCommand
-                    ]
-                ]
-            ]
-        ]]
+      $return[
+          $onlyIf[$or[$env[slash]==true;$env[slash]==false];Custom Function onlyForBotOwner isn't being used properly! Use true or false instead of $env[slash] in parameters.
+              $deleteIn[5s]]
+          $onlyIf[$authorID==$botOwnerID;
+              $ifx[
+                  $if[$env[slash==true;$ephemeral
+                      $color[#ff694b]
+                      $title[$toTitleCase[not authorized to use this command]]
+                      $description[Only $username[$botOwnerID] is authorized to use this command.
+                          But hey... why don't you come over tonight and i will authorize you something *else* 💋💋
+                      ]
+                  ]
+                  $else[
+                      $color[#ff694b]
+                      $title[$toTitleCase[not authorized to use this command]]
+                      $description[Only $username[$botOwnerID] is authorized to use this command.
+                          But hey... why don't you come over tonight and i will authorize you something *else* 💋💋
+                      ]$deleteIn[15s]
+                      $wait[15s]
+                      $deleteCommand
+                  ]
+              ]
+          ]
+      ]]
       `,
   },
 ];
