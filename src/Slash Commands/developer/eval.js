b77db8c16c229d20f130f7852650a95ed8245ad1
Aurea <94511889+Aurea6@users.noreply.github.com>
2025-01-08 21:19:42 +0530

    Update eval.js
    added comments
    removed useless bits

--- src/Slash Commands/developer/eval.js
+++ src/Slash Commands/developer/eval.js
@@ -1,19 +1,20 @@
 module.exports = {
-  code: `
-        $onlyForBotOwner[true]
-        $eval[$option[code]]
-        Ping: \`$pingMS\` | Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>
-      `,
+  code: `$c[---> Limiter]
+  $onlyForBotOwner[true]
+  $c[--->Execute the command]
+  $eval[$option[code]]
+  $c[--->Just a way to tell that the command was executed successfully in case there are no response]
+  Ping: \`$pingMS\` | Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>
+  `,
   data: {
-    name: "eval",
-    description: "runs the given code",
-    options: [
+    name: "eval", // name of the command
+    description: "runs the given code", // description of the command
+    options: [ // options
       {
-        type: 3,
-        name: "code",
-        description: "the code to run",
-        required: true,
-        choices: [],
+        type: 3, // text input type
+        name: "code", // name of the option
+        description: "the code to run", // the description of the option
+        required: true // is required?
       },
     ],
   },
